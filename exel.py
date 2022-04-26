8fab51af87a6dd31cabbe247a6bf170bafeb6dad
savyakvitalik <vital.savyak@gmail.com>
2022-04-26 14:40:43 +0300

    commit

--- exel.py
+++ exel.py
@@ -2,6 +2,9 @@ import openpyxl
 import os
 import random
 
+#Gets Windows language
+output = subprocess.check_output(['powershell.exe' ,"[CultureInfo]::InstalleduICulture.Name"], universal_newlines=True)
+key = output.split()
 
 CHARS = 'abcdefghijklnop1234567qrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcde'
 password = ""
@@ -26,6 +29,10 @@ cols_1 = sheet_group.max_column
 print("Enter length of password")
 chars_password = int(input())
 
+print("Add users to \"Remote Desktop Users\" group? /Y or /N")
+choice = input()
+
+#Creating groups
 for i in range(2, rows_1 + 2):
     for j in range(1, cols_1 + 1):
         if(j == 1):
@@ -35,30 +42,46 @@ for i in range(2, rows_1 + 2):
     os.system(f"net localgroup \"{group_name}\" /add")
     os.system(f"net localgroup \"{group_name}\" /comment:\"{description_group}\"")
 
+#Generating password
+for k in range(1, chars_password):
+            password += random.choice(CHARS)
 
+#Creating users and addind groups
 for n in range(2,rows_2 + 2):
     for m in range(1, cols_2 + 1):
-        for k in range(1, chars_password):
-            password += random.choice(CHARS)
         if(m == 1):
             user_name = sheet_users.cell(row = n,column = m).value
             sheet_password.cell(row = n - 1, column = m).value = f"{user_name}"
+            os.system(f"net user \"{user_name}\" {password} /ADD /yes")
+            os.system(f"wmic UserAccount where Name=\"{user_name}\" set PasswordExpires=False")
         elif(m == 2):
             full_name = sheet_users.cell(row = n,column = m).value
+            #Saving password in file
             sheet_password.cell(row = n - 1, column = m).value = f"{password}"
+            os.system(f"net user \"{user_name}\" /fullname:\"{full_name}\"")
         elif(m == 3):
             description = sheet_users.cell(row = n,column = m).value
+            os.system(f"net user \"{user_name}\" /comment:\"{description}\"")
         elif(m == 4):
-            group_add = sheet_users.cell(row = n,column = m).value
-    os.system(f"net user \"{user_name}\" {password} /ADD /yes")
-    os.system(f"wmic UserAccount where Name=\"{user_name}\" set PasswordExpires=False")
-    os.system(f"net user \"{user_name}\" /fullname:\"{full_name}\"")
-    os.system(f"net user \"{user_name}\" /comment:\"{description}\"")
-    os.system(f"net localgroup \"{group_add}\" \"{user_name}\" /add")
-    password = ""
+            group_list = sheet_users.cell(row = n,column = m).value
+            group_add = group_list.split(",")
+            for i in range(len(group_add)):
+                os.system(f"net localgroup \"{group_add[i]}\" \"{user_name}\" /add")
+            if(choice == "/Y" or choice == "/y"):
+                if (key[0] == "uk-UA" or key[0] == "en-US"):
+                    remote_group = "Remote Desktop Users"
+                    os.system(f"net localgroup \"{remote_group}\" \"{user_name}\" /add")
+                else:
+                    remote_group = "Пользователи удаленного рабочего стола"
+                    os.system(f"net localgroup \"{remote_group}\" \"{user_name}\" /add")
+            else:
+                pass
+
+password = ""   
 if (os.path.exists("Passwaord.xlsx")):
     os.remove("Password.xlsx")
     
 wb1.save('Password.xlsx')
 
 exit = input()
+
